101ae98046b8d9a848096822c62faec44d7c1295
sownyluv <snowyfe@proton.me>
2024-08-05 16:22:39 -0500

    database

--- db/db.go
+++ db/db.go
@@ -1,21 +1,49 @@
 package db
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func Ping() {
 	err := godotenv.Load()
 	if err != nil {
-		panic("Error loading.env file")
+		log.Fatal("Error loading .env file")
 	}
 	dburl := os.Getenv("DATABASE_URL")
 	if dburl == "" {
-		log.Fatalf("Missing DATABSE_URL environment variable")
+		log.Fatal("Set your 'DATABASE_URL' environment variable. " +
+			"See: " +
+			"www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
 	}
-	fmt.Println("Successfully connected!")
+
+	clientOptions := options.Client().ApplyURI(dburl)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			log.Fatalf("Failed to disconnect from MongoDB: %v", err)
+		}
+	}()
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("Failed to ping MongoDB: %v", err)
+	}
+
+	fmt.Println("Successfully Connected")
 }
